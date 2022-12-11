78d4598f9625f43e28c163d304b63d66564b0659
Cyph3rGh0stz <114602720+Cyph3rGh0stz@users.noreply.github.com>
2022-12-10 16:32:18 -0800

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#Openvpn install
+#Install Openvpn
 echo "Installing openvpn"
 sudo apt-get install -y Openvpn
+
+#Install ClamAV
+echo "Installing ClamAV"
+sudo apt-get install clamav clamav-daemon
 
 #
 echo "
