e3ab288decfe5965ef8853410ee21ad80cd91d77
Cyph3rGh0stz <114602720+Cyph3rGh0stz@users.noreply.github.com>
2022-12-10 16:08:06 -0800

    Update gettingstarted.sh

--- gettingstarted.sh
+++ gettingstarted.sh
@@ -31,30 +31,4 @@ sudo apt install default-jdk
 
 echo "Languages installed"
 sleep 1
-echo "Every good hacker needs some dev tools, here are some good options."
-
-#Terminator install
-sudo apt install -y terminator
-
-#Atom install
-echo "Installing the Atom text editor"
-wget "https://atom.io/download/deb" -O atom.deb
-dpkg -i atom.deb
-rm atom.deb
-
-#Visual Studio install
-echo "Installing Visual Studio Code"
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-
-#Install Docker
-sudo apt-get install docker.io
-
-echo "Developer tools installed."
-sleep 1
-echo "Script finished."
-sleep 1
-echo "Run security.sh to continue with setup."
+echo "Run security.sh to continue with setup"
