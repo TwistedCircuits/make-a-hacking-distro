f0a99861cd6fa981ce892eb14e560be902bf1aeb
Cyph3rGh0stz <114602720+Cyph3rGh0stz@users.noreply.github.com>
2022-12-12 07:35:58 -0800

    Update security.sh

--- security.sh
+++ security.sh
@@ -8,5 +8,18 @@ sudo apt-get install -y Openvpn
 echo "Installing ClamAV"
 sudo apt-get install clamav clamav-daemon
 
-#
-echo "
+#Install Keepass
+echo "Installing Keepass"
+sudo snap install keepassxc
+
+#Install Brave Browser
+echo "Installing Brave Browser"
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+#Install Anoncrypt
+echo "Installing Anoncrypt"
+
