9f0be84db095af66ea7f92e8a30208dd860069ae
Cyph3rGh0stz <114602720+Cyph3rGh0stz@users.noreply.github.com>
2022-12-16 23:06:19 -0700

    Update osintrecon.sh

--- osintrecon.sh
+++ osintrecon.sh
@@ -48,7 +48,6 @@ echo "Installing Spiderfoot"
 git clone https://github.com/smicallef/spiderfoot.git
 cd spiderfoot
 pip3 install -r requirements.txt
-python3 ./sf.py -l 127.0.0.1:5001
 cd ..
 
 #Maltego install
